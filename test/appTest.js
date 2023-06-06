70bc81035f1271e6803ad8416592e9a2b92177c0
Andrew Cho <cisdell@gmail.com>
2023-06-06 10:48:18 -0400

    fixed tests

--- test/appTest.js
+++ test/appTest.js
@@ -1,16 +1,20 @@
 const assert = require("chai").assert;
-const sayHello = require('../app').sayHello;
-// const app = require("../app");
+// const sayHello = require('../app').sayHello;
+const app = require("../app");
 
 
 describe('App', function() {
   it('app should return hello', function() {
-    let result = sayHello();
+    let result = app.sayHello();
     assert.equal(result, 'hello')
   });
 
   it('sayHello should return type string', function(){
-    let result = sayHello();
+    let result = app.sayHello();
     assert.typeOf(result, 'string');
+  })
+  it('addNumbers should be above 5', function() {
+    let result = app.addNumbers(5,5);
+    assert.isAbove(result, 5)
   })
 });
